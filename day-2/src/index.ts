8a1462574e5a751aca6fbebc59ec9cb7b482344f
Shane Yu <shanesimpkins@gmail.com>
2024-12-09 16:58:46 +0000

    feat: complete day 2 puzzle

---
+++ day-2/src/index.ts
@@ -0,0 +1,42 @@
+import { join } from 'node:path';
+
+const MAX_DIFFERENCE = 3;
+
+const input = await Bun.file(join(import.meta.dir, 'input.txt')).text();
+
+const reportData = input
+  .split('\n')
+  .map(line => line.trim().split(' ').map(Number));
+
+const allPositiveOrNegative = (data: number[]) =>
+  data.every(level => level > 0) || data.every(level => level < 0);
+
+const diffNoGreaterThanThree = (data: number[]) =>
+  data.every((level, i, arr) => i === 0 || Math.abs(level - arr[i - 1]) <= MAX_DIFFERENCE);
+
+const calculateDifferences = (data: number[]) =>
+  data.slice(1).map((level, i) => level - data[i]);
+
+const isValidAfterRemovingOne = (data: number[]) => {
+  for (let i = 0; i < data.length; i++) {
+    const filteredData = data.filter((_, index) => index !== i);
+    const differences = calculateDifferences(filteredData);
+
+    if (allPositiveOrNegative(differences) && diffNoGreaterThanThree(filteredData)) {
+      return true;
+    }
+  }
+
+  return false;
+};
+
+const safeReports = reportData.filter(data => {
+  const differences = calculateDifferences(data);
+
+  return (
+    (allPositiveOrNegative(differences) && diffNoGreaterThanThree(data)) ||
+    isValidAfterRemovingOne(data)
+  );
+}).length;
+
+console.log(`Total "safe" reports: ${safeReports}`);
