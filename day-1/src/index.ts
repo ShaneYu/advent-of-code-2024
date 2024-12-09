dabf910ecbd303f53ece9b3c2ea6d18b242e4c9b
Shane Yu <shanesimpkins@gmail.com>
2024-12-09 14:59:01 +0000

    feat: complete day 1 puzzle

---
+++ day-1/src/index.ts
@@ -0,0 +1,22 @@
+import { join } from 'node:path';
+
+const getSortedLists = (input: string) => input
+.trim()
+.split('\n')
+.map(line => line.trim().split('   ').map(Number))
+.reduce<[number[], number[]]>(
+  ([aList, bList], [a, b]) => ([ [...aList, a], [...bList, b] ]),
+  [[], []]
+)
+.map(list => list.toSorted((a, b) => a - b));
+
+
+const input = await Bun.file(join(import.meta.dir, 'input.txt')).text();
+const [listA, listB] = getSortedLists(input);
+const distances = listA.map((a, i) => Math.abs(a - listB[i]));
+
+const totalDistance = distances.reduce((acc, distance) => acc + distance, 0);
+const similarityScore = listA.map((a) => listB.filter(b => b === a).length * a).reduce((acc, score) => acc + score, 0);
+
+console.log(`Total Distance: ${totalDistance}`);
+console.log(`Similarity Score: ${similarityScore}`);
